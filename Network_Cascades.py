b55da7c0db8180be3285988155ba231b79287fa8
Jeff Alstott <jeffalstott@gmail.com>
2012-11-15 17:19:22 -0500

    Returned to functional programming style, where things are a little ugly but working.

--- Network_Cascades.py
+++ Network_Cascades.py
@@ -12,7 +12,8 @@ rc('font', family='serif')
 from scipy.io import loadmat
 from igraph import Graph
 from scipy.signal import correlate
-from Timelines import Timelines, Timeline
+from scipy.stats import kendalltau
+from numpy import mean, std, asarray
 import richclub
 
 # <codecell>
@@ -21,128 +22,94 @@ data_dir = "/data/alstottjd/Sini/"
 
 # <codecell>
 
-mat = loadmat(data_dir+'test_NL_mlast_nruns70_pinc0.001_addinc0_mnwt7_mxwt5_cf99_R1000000_netprms:_60_ws_-0.0333333-4_1_5_ncorr.mat')
-#mat = loadmat(data_dir+'test_NL_mlast_nruns70_pinc0.001_addinc0_mnwt7_mxwt5_cf99_R1000000_netprms:_60_oho_-6_1_5_ncorr.mat')
+#mat = loadmat(data_dir+'test_NL_mlast_nruns70_pinc0.001_addinc0_mnwt7_mxwt5_cf99_R1000000_netprms:_60_ws_-0.0333333-4_1_5_ncorr.mat')
+mat = loadmat(data_dir+'test_NL_mlast_nruns70_pinc0.001_addinc0_mnwt7_mxwt5_cf99_R1000000_netprms:_60_oho_-6_1_5_ncorr.mat')
 
 # <codecell>
 
 n_nets = shape(mat['pnets'])[1]
 n_runs = shape(mat['pnets'][0,0])[1]
 n_controls = shape(mat['pnets_spr_out'][0,0])[0]
-step_size = 50
+step_size = 10.0
 
-T_out = Timelines()
-T_in = Timelines()
+n_samples = ceil(n_runs/step_size)
+
+q_betweeness = zeros([n_nets, n_samples])
+n_betweeness = zeros([n_nets, n_samples])
+q_walktrap = zeros([n_nets, n_samples])
+n_walktrap = zeros([n_nets, n_samples])
+q_infomap = zeros([n_nets, n_samples])
+n_infomap = zeros([n_nets, n_samples])
+codelength = zeros([n_nets, n_samples])
+mean_path_length = zeros([n_nets, n_samples])
+mean_clustering = zeros([n_nets, n_samples])
+betweeness_change = zeros([n_nets, n_samples])
+
+rc_out = zeros([n_nets, n_samples, 9])
+rc_in = zeros([n_nets, n_samples, 9])
+rc_out_int = zeros([n_nets, n_samples])
+rc_in_int = zeros([n_nets, n_samples])
+
+node_size = 100
+alpha = .9
+width = .4
+
 for i in range(n_nets):
-    tl = Timeline()
-    CT_out = Timelines(with_control=False)
-    CT_in = Timelines(with_control=False)    
-    for j in arange(0,n_runs,step_size):
+    last_betweeness = 0
+    for j in arange(0,n_runs, step_size):
         if floor(j%100)==0:
-            print "Generation = %i"%j
+            print j
 
-        tl.add_gen(mat['pnets'][0,i][0,j].toarray().tolist())
+        g = Graph.Weighted_Adjacency(mat['pnets'][0,i][0,j].toarray().tolist())
         
-        if j==0:
-                c_out=[]
-                c_in=[]
-        for k in range(n_controls):
-            #print "k=%i"%k
-            if j==0:
-                c_out.append(Timeline())
-                c_in.append(Timeline())
-         
-            control_out = mat['pnets_spr_out'][i,j,k].toarray().tolist()
-            c_out[k].add_gen(control_out)
-            control_in = mat['pnets_spr_in'][i,j,k].toarray().tolist()
-            c_in[k].add_gen(control_in)
+        j = ceil(j/step_size)
         
-        CT_out.add_timelines(c_out)
-        CT_in.add_timelines(c_in)
+        #b = g.community_edge_betweenness(directed=True, weights=g.es["weight"])
+        #n_betweeness.append(b.optimal_count)
+        #q_betweeness.append(b.as_clustering().q)
         
-    T_out.add_timeline(tl, CT_out) 
-    T_in.add_timeline(tl, CT_in)
+        #w = g.community_walktrap(weights=g.es["weight"], steps=100)
+        #n_walktrap.append(w.optimal_count)
+        #q_walktrap.append(w.as_clustering().q)
         
+        infomap = g.community_infomap(edge_weights=g.es["weight"])
+        n_infomap[i,j] = infomap.cluster_graph().vcount()
+        q_infomap[i,j] = infomap.q
+        codelength[i, j] = infomap.codelength
 
-# <rawcell>
-
-# n_nets = shape(mat['pnets'])[1]
-# n_runs = shape(mat['pnets'][0,0])[1]
-# 
-# q_betweeness = zeros([n_nets, n_runs])
-# n_betweeness = zeros([n_nets, n_runs])
-# q_walktrap = zeros([n_nets, n_runs])
-# n_walktrap = zeros([n_nets, n_runs])
-# q_infomap = zeros([n_nets, n_runs])
-# n_infomap = zeros([n_nets, n_runs])
-# codelength = zeros([n_nets, n_runs])
-# mean_path_length = zeros([n_nets, n_runs])
-# mean_clustering = zeros([n_nets, n_runs])
-# betweeness_change = zeros([n_nets, n_runs])
-# 
-# rc_out = zeros([n_nets, n_runs, 9])
-# rc_in = zeros([n_nets, n_runs, 9])
-# rc_out_int = zeros([n_nets, n_runs])
-# rc_in_int = zeros([n_nets, n_runs])
-# close('all')
-# node_size = 100
-# alpha = .9
-# width = .4
-# 
-# for i in range(n_nets):
-#     last_betweeness = 0
-#     for j in arange(0,n_runs):#,10):
-#         if floor(j%50)==0:
-#             print j
-# 
-#         g = Graph.Weighted_Adjacency(mat['pnets'][0,i][0,j].toarray().tolist())
-#         
-#         
-#         #b = g.community_edge_betweenness(directed=True, weights=g.es["weight"])
-#         #n_betweeness.append(b.optimal_count)
-#         #q_betweeness.append(b.as_clustering().q)
-#         
-#         #w = g.community_walktrap(weights=g.es["weight"], steps=100)
-#         #n_walktrap.append(w.optimal_count)
-#         #q_walktrap.append(w.as_clustering().q)
-#         
-#         infomap = g.community_infomap(edge_weights=g.es["weight"])
-#         n_infomap[i,j] = infomap.cluster_graph().vcount()
-#         q_infomap[i,j] = infomap.q
-#         codelength[i, j] = infomap.codelength
-# 
-#         mean_path_length[i,j] = mean(g.shortest_paths(weights='weight'))
-#         mean_clustering[i,j] = mean(g.transitivity_local_undirected(weights='weight'))
-#         
-#         betweeness_sequence = g.edge_betweenness(weights='weight')
-#         if last_betweeness==0:
-#             last_betweeness = betweeness_sequence
-#         else:
-#             betweeness_change[i,j] = kendalltau(last_betweeness, betweeness_sequence)[0]
-#         
-#         rc_out[i,j,:] = rich_club_coefficient(g, scores_name='out_strength', control = mat['pnets_spr_out'][i,j,:])
-#         rc_in[i,j,:] = rich_club_coefficient(g, scores_name='in_strength', control = mat['pnets_spr_in'][i,j,:])
-#         rc_out_int[i,j] = sum(rc_out[i,j,:]-1)
-#         rc_in_int[i,j] = sum(rc_in[i,j,:]-1)
-#         
-#         
-#             
-#         if j in [0, 180, 480]:
-#             savetxt('inrichclub_frame%i.txt'%j, rc_in[i,j,:])
-#             savetxt('outrichclub_frame%i.txt'%j, rc_out[i,j,:])
-#             
-#             figure()
-#             plot(rc_in[i,j,:], 'b')
-#             plot(rc_out[i,j,:], 'g')
-#             #show()
-#             savefig('richclub_frame%i.pdf'%j)
-#             
-#         #net = nx.DiGraph(mat['pnets'][0,i][0,j])
-#         #pos=nx.spring_layout(net)
-#         #figure()
-#         #title(str(i)+', '+str(z.as_clustering().q))
-#         #nx.draw(net,pos,node_size=node_size,alpha=alpha, width=width, with_labels=False)
-#         #show()
+        mean_path_length[i,j] = mean(g.shortest_paths(weights='weight'))
+        mean_clustering[i,j] = mean(g.transitivity_local_undirected(weights='weight'))
+        
+        betweeness_sequence = g.edge_betweenness(weights='weight')
+        if last_betweeness==0:
+            last_betweeness = betweeness_sequence
+        else:
+            betweeness_change[i,j] = kendalltau(last_betweeness, betweeness_sequence)[0]
+        
+        rc_out[i,j,:] = richclub.rich_club_coefficient(g, scores_name='out_strength', control = mat['pnets_spr_out'][i,j,:])
+        rc_in[i,j,:] = richclub.rich_club_coefficient(g, scores_name='in_strength', control = mat['pnets_spr_in'][i,j,:])
+        rc_out_int[i,j] = sum(rc_out[i,j,:]-1)
+        rc_in_int[i,j] = sum(rc_in[i,j,:]-1)
+        
+        
+            
+        if j in [0, 180, 480]:
+            savetxt('inrichclub_frame%i.txt'%j, rc_in[i,j,:])
+            savetxt('outrichclub_frame%i.txt'%j, rc_out[i,j,:])
+            
+            figure()
+            plot(rc_in[i,j,:], 'b')
+            plot(rc_out[i,j,:], 'g')
+            #show()
+            savefig('richclub_frame%i.pdf'%j)
+            
+        #net = nx.DiGraph(mat['pnets'][0,i][0,j])
+        #pos=nx.spring_layout(net)
+        #figure()
+        #title(str(i)+', '+str(z.as_clustering().q))
+        #nx.draw(net,pos,node_size=node_size,alpha=alpha, width=width, with_labels=False)
+        #show()
+        
 
 # <codecell>
 
@@ -153,10 +120,13 @@ h = w/1.6180
 #f = figure(figsize=(w,h))
 f = figure()
 
+s1 = 0
+s2 = floor((180.0/n_runs)*n_samples)
+s3 = floor((480.0/n_runs)*n_samples)
+
 ax1 = f.add_subplot(131)
-handles = {}
-handles['In Strength'] = ax1.plot(x_vals, T_in.rc_in[0]rc_in[0,0, :], label='In Strength Rich Club', color='b')
-handles['In Strength'] = ax1.plot(x_vals, rc_out[0,0,:], label='Out Strength Rich Club', color='g')
+ax1.plot(x_vals, rc_in[0,s1, :], label='In Strength', color='b')
+ax1.plot(x_vals, rc_out[0,s1,:], label='Out Strength', color='g')
 plt.setp(ax1.get_xticklabels(), visible=False)
 ylabel("Rich Club Coefficient")
 text(.5, .9, '0 Cascades', transform = ax1.transAxes, horizontalalignment='center', fontsize=10)
@@ -166,23 +136,21 @@ plt.xticks(x_vals[::2], x_vals[::2])
 #ax1.legend(handles, labels, loc=6)
 
 ax2 = f.add_subplot(132, sharey=ax1)
-handles = {}
-handles['In Strength'] = ax2.plot(x_vals, rc_in[0,180,:], label='In Strength Rich Club', color='b')
-handles['In Strength'] = ax2.plot(x_vals, rc_out[0,180,:], label='Out Strength Rich Club', color='g')
+ax2.plot(x_vals, rc_in[0,s2,:], label='In Strength', color='b')
+ax2.plot(x_vals, rc_out[0,s2,:], label='Out Strength', color='g')
 plt.setp(ax2.get_yticklabels(), visible=False)
 xlabel('Strength Decile')
 plt.xticks(x_vals[::2], (x_vals[::2]*100).astype(int))
-text(.5, .9, '36x10$^{5}$ Cascades', transform = ax2.transAxes, horizontalalignment='center', fontsize=10)
+text(.5, .9, '3.6x10$^{6}$ Cascades', transform = ax2.transAxes, horizontalalignment='center', fontsize=10)
 #handles, labels = ax.get_legend_handles_labels()
 #ax.legend(handles, labels, loc=1)
 
 ax3 = f.add_subplot(133, sharey=ax1)
-handles = {}
-handles['In Strength'] = ax3.plot(x_vals, rc_in[0,480,:], label='In Strength', color='b')
-handles['In Strength'] = ax3.plot(x_vals, rc_out[0,480,:], label='Out Strength', color='g')
+ax3.plot(x_vals, rc_in[0,s3,:], label='In Strength', color='b')
+ax3.plot(x_vals, rc_out[0,s3,:], label='Out Strength', color='g')
 plt.setp(ax3.get_yticklabels(), visible=False)
 plt.setp(ax3.get_xticklabels(), visible=False)
-text(.5, .9, '96x10$^{5}$ Cascades', transform = ax3.transAxes, horizontalalignment='center', fontsize=10)
+text(.5, .9, '9.6x10$^{6}$ Cascades', transform = ax3.transAxes, horizontalalignment='center', fontsize=10)
 #xlabel('Strength Decile')
 plt.xticks(x_vals[::2], x_vals[::2])
 
@@ -204,17 +172,17 @@ x_vals = 10.0*x_vals/n_runs
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
 #Modularity Metrics
-#plot(n_betweeness)
 figure()
-#plot(q_betweeness)
-y_vals, error  = T_out.__getattr__("n_infomap", version='raw')
+y_vals = mean(n_infomap, axis=0)
+error = std(n_infomap, axis=0)
 plot(x_vals, y_vals)
 fill_between(x_vals, y_vals-error, y_vals+error, alpha=.5)
 
 xlabel("Cascades (n x 10$^{6}$)")
 ylabel("Number of Modules")
 inset_ax = axes([0.58,0.58,0.3,0.3])
-y_vals, error  = T_out.__getattr__("q_infomap", version='raw')
+y_vals = mean(q_infomap, axis=0)
+error = std(q_infomap, axis=0)
 inset_ax.plot(x_vals, y_vals)
 inset_ax.fill_between(x_vals, y_vals-error, y_vals+error, alpha=.5)
 ylabel("Modularity Index", fontsize=10)
@@ -229,7 +197,8 @@ savefig(data_dir+'Modularity.pdf', bbox_inches='tight')
 # <codecell>
 
 figure()
-y_vals, error = T_out.__getattr__("codelength", version='normalized')
+y_vals = mean(codelength, axis=0)
+error = std(codelength, axis=0)
 plot(x_vals, y_vals)
 fill_between(x_vals, y_vals-error, y_vals+error, alpha=.5)
 
@@ -245,11 +214,13 @@ savefig(data_dir+'Compression.pdf', bbox_inches='tight')
 f = figure()
 ax = f.add_subplot(111)
 handles = {}
-y_vals, error = T_in.__getattr__("rc_in_int", version='raw')
+y_vals = mean(rc_in_int, axis=0)
+error = std(rc_in_int, axis=0)
 handles['In Strength'] = ax.plot(x_vals, y_vals, label='In Strength', color='b')
 fill_between(x_vals, y_vals-error, y_vals+error, color='b', alpha=.5)
 
-y_vals, error = T_out.__getattr__("rc_out_int", version='raw')
+y_vals = mean(rc_out_int, axis=0)
+error = std(rc_out_int, axis=0)
 handles['Out Strength'] = ax.plot(x_vals, y_vals, label='Out Strength', color='g')
 fill_between(x_vals, y_vals-error, y_vals+error, color='g', alpha=.5)
 
@@ -270,11 +241,13 @@ savefig(data_dir+'RichClubInt.pdf', bbox_inches='tight')
 f = figure()
 ax = f.add_subplot(111)
 handles = {}
-y_vals, error = T_out.mean_path_length
+y_vals = mean(mean_path_length, axis=0)
+error = std(mean_path_length, axis=0)
 handles['In Strength'] = ax.plot(x_vals, y_vals, label='Path Length', color='b')
 fill_between(x_vals, y_vals-error, y_vals+error, color='b', alpha=.5)
 
-y_vals, error = T_out.mean_clustering
+y_vals = mean(mean_clustering, axis=0)
+error = std(mean_clustering, axis=0)
 handles['Out Strength'] = ax.plot(x_vals, y_vals, label='Clustering', color='g')
 fill_between(x_vals, y_vals-error, y_vals+error, color='g', alpha=.5)
 
@@ -293,7 +266,8 @@ ax.legend(handles, labels, loc=1)
 # <codecell>
 
 figure()
-y_vals, error = T_out.__getattr__("betweeness_change_kendall", version='raw')
+y_vals = mean(betweeness_change, axis=0)[1:]
+error = std(betweeness_change, axis=0)[1:]
 plot(x_vals[1:], y_vals)
 fill_between(x_vals[1:], y_vals-error, y_vals+error, alpha=.5)
 
