511b476e253ab0abc18581eaee7b13e5f4d88326
Jeff <jeffalstott@gmail.com>
2012-11-27 13:17:14 +0000

    Improved Timelines, I hope.

--- Timelines.py
+++ Timelines.py
@@ -1,6 +1,6 @@
 from igraph import Graph
 from scipy.stats import kendalltau, spearmanr
-from numpy import mean, std, asarray
+from numpy import mean, std, empty
 import richclub
 
 
@@ -24,33 +24,56 @@ class Timelines:
             for i in control_tls:
                 self.controls.append(i)
 
-    def __getattr__(self, name, version=None):
+    def raw_data(self, name):
+        n_tls = len(self.tls)
+        raw_data = empty(n_tls,
+            dtype=type(getattr(self.tls[0], name)))
 
-        if version is None:
-            if self.control:
-                version = 'normalized'
-            else:
-                version = 'raw'
+        for i in range(n_tls):
+            raw_data[i] = getattr(self.tls[i], name)
 
-        if version in ['normalized', 'raw']:
-            raw = []
-            for i in self.tls:
-                raw.append(getattr(i, name))
-        if version in ['normalized', 'control']:
-            control = []
-            for i in self.controls:
-                control.append(getattr(i, name)[0])
+        return raw_data
 
-        if version == 'raw':
-            m = mean(asarray(raw), axis=0)
-            s = std(asarray(raw), axis=0)
-        elif version == 'control':
-            m = mean(asarray(control), axis=0)
-            s = std(asarray(control), axis=0)
-        elif version == 'normalized':
-            data = asarray(raw) / asarray(control)
-            m = mean(data, axis=0)
-            s = std(data, axis=0)
+    def raw_control(self, name):
+        n_tls = len(self.tls)
+        raw_control = empty(n_tls,
+            dtype=type(self.controls[0].raw(name)))
+
+        for i in range(n_tls):
+            raw_control[i] = self.controls[i].raw(name)
+
+        return raw_control
+
+    def data(self, name):
+        raw_data = self.raw_data(name)
+        m = mean(raw_data, axis=0)
+        s = std(raw_data, axis=0)
+
+        return m, s
+
+    def control(self, name):
+        raw_control = self.raw_control(name)
+        raw_control = mean(raw_control, axis=1)
+        m = mean(raw_control, axis=0)
+        s = std(raw_control, axis=0)
+
+        return m, s
+
+    def raw_normalized(self, name):
+
+        #each data point should be paired to its own control
+        raw_data = self.raw_data(name)
+        raw_control = self.raw_control(name)
+        raw_control = mean(raw_control, axis=1)
+        raw_normalized = raw_data / raw_control
+
+        return raw_normalized
+
+    def normalized(self, name):
+        raw_normalized = self.raw_normalized(name)
+
+        m = mean(raw_normalized, axis=0)
+        s = std(raw_normalized, axis=0)
 
         return m, s
 
@@ -109,3 +132,17 @@ class Timeline:
         self.rc_in.append(richclub.rich_club_coefficient(g, scores_name='in_strength', rewire=False))
         self.rc_out_int.append(sum(self.rc_out[-1] - 1))
         self.rc_in_int.append(sum(self.rc_in[-1] - 1))
+
+    def close_gens(self):
+        from numpy import asarray
+        self.n_infomap = asarray(self.n_infomap)
+        self.q_infomap = asarray(self.q_infomap)
+        self.codelength = asarray(self.codelength)
+        self.mean_path_length = asarray(self.mean_path_length)
+        self.mean_clustering = asarray(self.mean_clustering)
+        self.betweeness_change_kendall = asarray(self.betweeness_change_kendall)
+        self.betweeness_change_spearmanr = asarray(self.betweeness_change_spearmanr)
+        self.rc_out = asarray(self.rc_out)
+        self.rc_in = asarray(self.rc_in)
+        self.rc_out_int = asarray(self.rc_out_int)
+        self.rc_in_int = asarray(self.rc_in_int)
