3055a8f8a90bb99b391a452d88c4a29f661d26b8
Jeff Alstott <jeffalstott@gmail.com>
2012-11-28 12:20:49 -0500

    Finished converting current figures to include other learning rules.

--- Network Cascade Figures.py
+++ Network Cascade Figures.py
@@ -25,36 +25,79 @@ n_iters = 5
 
 # <codecell>
 
-data = {}
+datanames = [#'mlast',
+ #'mL1',
+ #'mL2',
+ #'mL3',
+ #'mL4',
+ #'mL5',
+ 'mall',
+ 'mlast',
+ 'mo-2',
+ 'mo-1',
+ #'mo-2',
+ #'mo-3',
+ #'mo-4',
+ #'mo-5',
+]
+
+# <codecell>
+
+data = {
+#'mlast': {'name': 'Last', 'color': 'k', 'line': '-'},
+'mlast': {'name': 'Last', 'color': 'k', 'line': '-'},
+'mL1': {'name': 'L1', 'color': 'r', 'line': '-'},
+'mL2': {'name': 'L2', 'color': 'g', 'line': '-'},
+'mL3': {'name': 'L3', 'color': 'b', 'line': '-'},
+'mL4': {'name': 'L4', 'color': 'c', 'line': '-'},
+'mL5': {'name': 'L5', 'color': 'm', 'line': '-'},
+#'mall': {'name': 'All', 'color': 'k', 'line': '--'},
+'mall': {'name': 'All', 'color': 'b', 'line': '-'},
+#'mo-1': {'name': 'F1', 'color': 'r', 'line': '--'},
+'mo-1': {'name': 'Random', 'color': 'r', 'line': '-'},
+#'mo-2': {'name': 'F2', 'color': 'g', 'line': '--'},
+'mo-2': {'name': 'Second', 'color': 'g', 'line': '-'},
+'mo-3' : {'name': 'F3', 'color': 'b', 'line': '--'},
+'mo-4': {'name': 'F4', 'color': 'c', 'line': '--'},
+'mo-5': {'name': 'F5', 'color': 'm', 'line': '--'}
+}
+
+# <codecell>
+
 for filename in files:
     if filename.startswith('tl_'+str(n_rewires)+','+str(n_iters)):
         dataname = filename.split('_')[3]
-        data[dataname] = pickle.load( open( data_directory+filename, "rb" ))
+        if dataname in datanames:
+            data[dataname]['data'] = pickle.load( open( data_directory+filename, "rb" ))
 
 # <codecell>
 
 n_runs = 500
 step_size = 5
 close('all')
-x_vals = arange(0, n_runs, step_size)
+x_vals = arange(0, n_runs+1, step_size)
 x_vals = 10.0*x_vals/n_runs
+alpha = .05
 
 # <codecell>
 
-figure()
+f = figure()
+ax = f.add_subplot(111)
 
-for i in data:
-    y_vals, error = data[i]["T_out"].data("codelength")
-    plot(x_vals, y_vals, label=i)
-#    fill_between(x_vals, y_vals-error, y_vals+error, alpha=.5)
+for i in datanames:
+    y_vals, error = data[i]['data']["T_out"].data("codelength")
+    ax.plot(x_vals, y_vals, 
+        label=data[i]['name'], color=data[i]['color'], linestyle=data[i]['line'])
+    fill_between(x_vals, y_vals-error, y_vals+error, alpha=alpha, color=data[i]['color'])
 
 handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles, labels, loc=1)
+#ax.legend(handles, labels, loc=1)
+ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
 xlabel("Cascades (n x 10$^{6}$)")
 ylabel("Bits to Represent Network")
 
-savefig(data_dir+'Compression.pdf', bbox_inches='tight')
+savefig(data_directory+'Compression.pdf', bbox_inches='tight')
 #title("Network Compression with Learning")
 
 # <codecell>
@@ -62,26 +105,202 @@ savefig(data_dir+'Compression.pdf', bbox_inches='tight')
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
 #Modularity Metrics
-figure()
-for i in data:
-    y_vals, error = data[i]["T_out"].data("q_infomap")
-    plot(x_vals, y_vals, label=i)
-#    fill_between(x_vals, y_vals-error, y_vals+error, alpha=.5)
+f = figure()
+ax = f.add_subplot(111)
 
+for i in datanames:
+    y_vals, error = data[i]['data']["T_out"].data("q_infomap")
+    ax.plot(x_vals, y_vals, 
+        label=data[i]['name'], color=data[i]['color'], linestyle=data[i]['line'])
+    fill_between(x_vals, y_vals-error, y_vals+error, alpha=alpha, color=data[i]['color'])
+
+ax.set_ylim(0,.701)
 xlabel("Cascades (n x 10$^{6}$)")
-ylabel("Number of Modules")
+ylabel("Modularity Index")
 handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles, labels, loc=2)
+#ax.legend(handles, labels, loc=1)
+ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
-inset_ax = axes([0.58,0.58,0.3,0.3])
-for i in data:
-    y_vals, error = data[i]["T_out"].data("n_infomap")
-    inset_axplot(x_vals, y_vals, label=i)
-    #inset_ax.fill_between(x_vals, y_vals-error, y_vals+error, alpha=.5)
-ylabel("Modularity Index", fontsize=10)
-inset_ax.yaxis.set_major_locator(MultipleLocator(.1))
+inset_ax = axes([0.58,0.2,0.3,0.3])
+for i in datanames:
+    y_vals, error = data[i]['data']["T_out"].data("n_infomap")
+    inset_ax.plot(x_vals, y_vals, 
+        label=data[i]['name'], color=data[i]['color'], linestyle=data[i]['line'])
+    inset_ax.fill_between(x_vals, y_vals-error, y_vals+error, alpha=alpha, color=data[i]['color'])
+ylabel("Number of Modules", fontsize=10)
+#inset_ax.yaxis.set_major_locator(MultipleLocator(.1))
 
 
-savefig(data_dir+'Modularity.pdf', bbox_inches='tight')
+savefig(data_directory+'Modularity.pdf', bbox_inches='tight')
 #title("Modularity with Learning")
+
+# <codecell>
+
+f = figure()
+ax = f.add_subplot(111)
+ax.spines['top'].set_color('none')
+ax.spines['bottom'].set_color('none')
+ax.spines['left'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
+ax.set_xlabel("Cascades (n x 10$^{6}$)")
+ax.set_ylabel("Integrated Rich Club Index")
+
+ax1 = f.add_subplot(211)
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+ax1.yaxis.set_major_locator(MultipleLocator(10))
+ax2 = f.add_subplot(212, sharey=ax1, sharex=ax1)
+plt.setp(ax1.get_xticklabels(), visible=False)
+
+ax1.text(.5, .8, 'In Strength',
+ transform = ax1.transAxes, horizontalalignment='center', fontsize=10)
+ax2.text(.5, .8, 'Out Strength',
+ transform = ax2.transAxes, horizontalalignment='center', fontsize=10)
+
+ax1.set_xlim(x_vals[0], x_vals[-1])
+#ax1.set_ylim(-5, 40)
+ax1.plot(ax1.get_xlim(), (0,0), 'k')
+ax2.plot(ax2.get_xlim(), (0,0), 'k')
+
+for i in datanames:
+    y = sum(data[i]['data']["T_in"].raw_normalized("rc_in")-1, axis=-1)
+    y_vals = mean(y, axis=0)
+    error = std(y, axis=0)
+    ax1.plot(x_vals, y_vals, 
+        label=data[i]['name'], color=data[i]['color'], linestyle=data[i]['line'])
+    ax1.fill_between(x_vals, y_vals-error, y_vals+error, alpha=alpha, color=data[i]['color'])
+    
+    y = sum(data[i]['data']["T_out"].raw_normalized("rc_out")-1, axis=-1)
+    y_vals = mean(y, axis=0)
+    error = std(y, axis=0)
+    ax2.plot(x_vals, y_vals, 
+        label=data[i]['name'], color=data[i]['color'], linestyle=data[i]['line'])
+    ax2.fill_between(x_vals, y_vals-error, y_vals+error, alpha=alpha, color=data[i]['color'])
+
+ax1.plot(ax1.get_xlim(), (0,0), 'k')
+ax2.plot(ax2.get_xlim(), (0,0), 'k')
+
+
+
+handles, labels = ax2.get_legend_handles_labels()
+ax.legend(handles, labels, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+savefig(data_directory+'RichClubInt.pdf', bbox_inches='tight')
+#title("Rich Club with Learning")
+
+# <codecell>
+
+f = figure()
+ax = f.add_subplot(111)
+ax.spines['top'].set_color('none')
+ax.spines['bottom'].set_color('none')
+ax.spines['left'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
+ax.set_xlabel("Cascades (n x 10$^{6}$)")
+ax.set_ylabel("Integrated Rich Club Index")
+
+ax1 = f.add_subplot(121)
+ax2 = f.add_subplot(122, sharey=ax1, sharex=ax1)
+plt.setp(ax2.get_yticklabels(), visible=False)
+
+ax1.text(.5, .8, 'In Strength',
+ transform = ax1.transAxes, horizontalalignment='center', fontsize=10)
+ax2.text(.5, .8, 'Out Strength',
+ transform = ax2.transAxes, horizontalalignment='center', fontsize=10)
+
+ax1.set_xlim(x_vals[0], x_vals[-1])
+ax1.plot(ax1.get_xlim(), (0,0), 'k')
+ax2.plot(ax2.get_xlim(), (0,0), 'k')
+
+for i in datanames:
+    y = sum(data[i]['data']["T_in"].raw_normalized("rc_in")-1, axis=-1)
+    y_vals = mean(y, axis=0)
+    error = std(y, axis=0)
+    ax1.plot(x_vals, y_vals, 
+        label=data[i]['name'], color=data[i]['color'], linestyle=data[i]['line'])
+    ax1.fill_between(x_vals, y_vals-error, y_vals+error, alpha=alpha, color=data[i]['color'])
+    
+    y = sum(data[i]['data']["T_out"].raw_normalized("rc_out")-1, axis=-1)
+    y_vals = mean(y, axis=0)
+    error = std(y, axis=0)
+    ax2.plot(x_vals, y_vals, 
+        label=data[i]['name'], color=data[i]['color'], linestyle=data[i]['line'])
+    ax2.fill_between(x_vals, y_vals-error, y_vals+error, alpha=alpha, color=data[i]['color'])
+
+ax1.plot(ax1.get_xlim(), (0,0), 'k')
+ax2.plot(ax2.get_xlim(), (0,0), 'k')
+
+handles, labels = ax2.get_legend_handles_labels()
+ax.legend(handles, labels, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+savefig(data_directory+'RichClubInt.pdf', bbox_inches='tight')
+#title("Rich Club with Learning")
+
+# <codecell>
+
+f = figure()
+ax = f.add_subplot(111)
+ax.spines['top'].set_color('none')
+ax.spines['bottom'].set_color('none')
+ax.spines['left'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
+ax.set_xlabel("Cascades (n x 10$^{6}$)")
+
+ax1 = f.add_subplot(131)
+ax1.set_ylabel('Mean Path Length')
+ax2 = f.add_subplot(132, sharey=ax1, sharex=ax1)
+ax2.set_ylabel('Mean Clustering')
+plt.setp(ax2.get_yticklabels(), visible=False)
+ax3 = f.add_subplot(133, sharey=ax1, sharex=ax1)
+ax3.set_ylabel('Small World Index')
+plt.setp(ax3.get_yticklabels(), visible=False)
+
+ax1.set_xlim(x_vals[0], x_vals[-1])#len(x_vals)*2/3])
+
+for i in datanames:
+    y_vals, error = data[i]['data']["T_out"].normalized("mean_path_length")
+    ax1.plot(x_vals, y_vals, 
+        label=data[i]['name'], color=data[i]['color'], linestyle=data[i]['line'])
+    ax1.fill_between(x_vals, y_vals-error, y_vals+error, alpha=alpha, color=data[i]['color'])
+    
+    y_vals, error = data[i]['data']["T_out"].normalized("mean_clustering")
+    ax2.plot(x_vals, y_vals, 
+        label=data[i]['name'], color=data[i]['color'], linestyle=data[i]['line'])
+    ax2.fill_between(x_vals, y_vals-error, y_vals+error, alpha=alpha, color=data[i]['color'])
+    
+    y_vals = data[i]['data']["T_out"].normalized("mean_clustering")[0]/data[i]['data']["T_out"].normalized("mean_path_length")[0]
+    ax3.plot(x_vals, y_vals, 
+        label=data[i]['name'], color=data[i]['color'], linestyle=data[i]['line'])
+
+handles, labels = ax1.get_legend_handles_labels()
+#ax.legend(handles, labels, loc=1)
+ax.legend(handles, labels, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+savefig(data_directory+'PathLengthClustering.pdf', bbox_inches='tight')
+
+# <codecell>
+
+f = figure()
+ax = f.add_subplot(111)
+
+for i in datanames:
+    y_vals, error = data[i]['data']["T_out"].data("betweeness_change_kendall")
+    ax.plot(x_vals[1:], y_vals, 
+        label=data[i]['name'], color=data[i]['color'], linestyle=data[i]['line'])
+    ax.fill_between(x_vals[1:], y_vals-error, y_vals+error, alpha=alpha, color=data[i]['color'])
+
+    y_vals, error = data[i]['data']["T_out"].control("betweeness_change_kendall")
+    ax.plot(x_vals[1:], y_vals, color=data[i]['color'], linestyle=data[i]['line']+'.')
+#        label=data[i]['name'], 
+    #ax.fill_between(x_vals[1:], y_vals-error, y_vals+error, alpha=alpha, color=data[i]['color'])
+    
+handles, labels = ax.get_legend_handles_labels()
+#ax.legend(handles, labels, loc=1)
+ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+ax.set_xlabel("Cascades (n x 10$^{6}$)")
+ax.set_ylabel("Correlation in Betweeness Centrality Rank Order")
+
+savefig(data_directory+'Rerouting.pdf', bbox_inches='tight')
+#title("Network Compression with Learning")
 
